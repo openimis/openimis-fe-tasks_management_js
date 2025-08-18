5842caaf0f4b825a4b970ae7bc95d0dd9cf7e9b2
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-08-19 00:58:14 +0530

    fix(build): correct file extension

--- vite.config.js
+++ vite.config.js
@@ -56,7 +56,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/index.jsx'),
       name: 'TasksManagement',
-      fileName: (format) => `index.${format}.js`,
+      fileName: (format) => `index.${format === 'es' ? 'es' : 'cjs'}.js`,
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
